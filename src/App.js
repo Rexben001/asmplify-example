fab8ee1c320cce758ec84c5f15d16c94b50e657f
Rexben <rexben.rb@gmail.com>
2020-10-19 14:24:26 +0100

    add signup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { withAuthenticator } from 'aws-amplify-react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
@@ -23,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
